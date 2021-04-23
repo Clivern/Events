a0fe943f98a6671d4eb036f8cb937cbf01f44115
Clivern <hello@clivern.com>
2021-04-24 00:29:33 +0200

    fixes

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use OpenApi\Annotations as OA;
 use App\Repository\UserRepository;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
